795b9098cb598fda0e7b979d18df247dba25939b
Dylan Carver <dylancarver14@gmail.com>
2023-02-28 21:28:28 -0600

    First Commit

---
+++ testapi/src/test/java/com/demoapi/testapi/TestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoapi.testapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
